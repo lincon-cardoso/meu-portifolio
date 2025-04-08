880ef4dbf1622789ce759aa954d104408f197275
Lincon <linkon789@gmail.com>
2025-04-07 23:23:21 -0300

    adiciona configuração para geração de sitemap e robots.txt, atualiza dependências no package.json e tsconfig.json

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.devlincon.com.br", // 👉 substitua pelo seu domínio
+  generateRobotsTxt: true, // gera robots.txt também
+  sitemapSize: 7000,
+  changefreq: "monthly",
+  priority: 0.7,
+};
