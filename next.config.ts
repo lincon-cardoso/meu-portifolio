e7a23d3667dd8accdb85e08e574a7494fc1334b3
Lincon <linkon789@gmail.com>
2025-04-08 22:36:26 -0300

    Remove unused dependency 'critters' from package.json

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 
   experimental: {
-    optimizeCss: true,
+    optimizeCss: true, // Ativa otimização de CSS
   },
 
   async headers() {
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
           { key: "X-Frame-Options", value: "DENY" },
           { key: "X-XSS-Protection", value: "1; mode=block" },
-          { key: "Surrogate-Control", value: "no-store" }
+          { key: "Surrogate-Control", value: "no-store" },
         ],
       },
 
@@ -38,9 +38,11 @@ const nextConfig: NextConfig = {
         headers: [
           { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
         ],
-      }
+      },
     ];
   },
+
+
 };
 
 export default nextConfig;
