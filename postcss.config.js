5d34593b46c7a2ae60a6eeb509bd4dc75871c5f7
Lincon <linkon789@gmail.com>
2025-04-08 21:16:17 -0300

    refactor: ajusta a configuração do PostCSS para incluir plugins e corrigir a estrutura

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const purgecss = require("@fullhuman/postcss-purgecss").default;
-
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   plugins: [
+    require("postcss-flexbugs-fixes"),
+    require("postcss-preset-env")({
+      stage: 3,
+      features: {
+        "custom-properties": false,
+      },
+    }),
     ...(isProd
       ? [
           purgecss({
@@ -13,6 +18,7 @@ module.exports = {
               "./components/**/*.{js,ts,jsx,tsx}",
               "./app/**/*.{js,ts,jsx,tsx}",
             ],
+            css: ["./styles/**/*.scss"],
             safelist: [/^fade-/, /^btn-/, /^nav-/, /^MeuPortifolio/],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
