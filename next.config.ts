5be94f24c2c76cf3f01ebb3f5d0e6ec9c5a680a3
Lincon <linkon789@gmail.com>
2025-04-13 12:48:02 -0300

    fix: comenta cabeçalhos de Content-Security-Policy no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -22,11 +22,7 @@ const nextConfig: NextConfig = {
           { key: "X-XSS-Protection", value: "1; mode=block" },
           { key: "Surrogate-Control", value: "no-store" },
           { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains; preload" },
-          // { key: "Content-Security-Policy", value: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src * blob: data:; connect-src *; frame-ancestors 'none';" },
-          // {
-          //   key: "Content-Security-Policy",
-          //   value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src * blob: data:; connect-src *; frame-ancestors 'none';"
-          // },
+
           { key: "X-Permitted-Cross-Domain-Policies", value: "none" },
 
           { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
