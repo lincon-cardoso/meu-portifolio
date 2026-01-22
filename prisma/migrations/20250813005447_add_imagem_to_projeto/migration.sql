698a8e8c9583700a95049538c9df4622b847d19f
Lincon <linkon789@gmail.com>
2026-01-21 23:02:56 -0300

    refactor: update package.json scripts to use npx for prisma commands and upgrade dependencies
    chore: remove unused migration files and prisma configuration
    
    refactor: clean up schema.prisma and related migration files
    
    fix: update Home component to reflect page reconstruction status

--- prisma/migrations/20250813005447_add_imagem_to_projeto/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `imagem` on the `Projeto` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Projeto" DROP COLUMN "imagem";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,16 @@
 import HomePage from "@/components/home/HomePage";
-import Cabecalho from "@/components/layout/Cabecalho"; // Ensure that the file exists at this path
-import Rodape from "@/components/layout/Rodape"; // Ensure that the file exists at this path
+import Cabecalho from "@/components/layout/Cabecalho";
+import Rodape from "@/components/layout/Rodape";
 import type { Metadata } from "next";
 
 export default function Home() {
   return (
     <main id="main" role="main">
-{/* 
-      {/* Cabeçalho Global */}
-
-      <Cabecalho />
-
-      <HomePage />
-
-      {/* Rodapé Global */}
-      <Rodape />
-*/}
-<p>Página em Reconstrução</p>
+      Pagina em reconstrução...
     </main>
   );
 }
+
 
 export const metadata: Metadata = {
   title: "Portfólio | Lincon Cardoso",
